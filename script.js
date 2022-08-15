c3056c820f10e165d6ca932035b8d08efcdd0168
Sahaspuria <official.faizan.alam@gmail.com>
2022-08-16 01:37:55 +0530

    Project Upload

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const opnModal = document.querySelectorAll('.show-modal');
+const clsModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < opnModal.length; i++) {
+  opnModal[i].addEventListener('click', openModal);
+}
+
+clsModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
